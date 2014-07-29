fe00653f52ebd2bd4a3438916324c073a8a2c7f3
Henry <cryptix@riseup.net>
2014-07-29 15:26:52 +0200

    replaced regexp code with goQuery

--- parsers.go
+++ parsers.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"regexp"
 	"strconv"
 	"strings"
-)
 
-var (
-	artNrRegexp = regexp.MustCompile(`\(Art.Nr.([0-9]*)\)<td class="spalteArtikel2" align="right" valign="middle">[0-9/]*k?g</td><td align="right" valign="middle">([0-9]*\.[0-9]*).</td><td class="spalteArtikel4`)
+	"github.com/PuerkitoBio/goquery"
 )
 
 func kategorieWorker(client *http.Client, jobs <-chan string) <-chan Artikel {
@@ -29,46 +25,76 @@ func kategorieWorker(client *http.Client, jobs <-chan string) <-chan Artikel {
 			}
 			defer resp.Body.Close()
 
-			body, err := ioutil.ReadAll(resp.Body)
+			doc, err := goquery.NewDocumentFromResponse(resp)
 			if err != nil {
-				log.Printf("KatWorker[%s] Error:%s", kat, err)
+				log.Printf("KatWorker[%s] NewDocumentFromResponse Error:%s", kat, err)
 				break
 			}
 
-			// find all the products
-			for _, match := range artNrRegexp.FindAllStringSubmatch(string(body), -1) {
-				if len(match) > 3 {
-					log.Printf("len(match) > 3 ! %v\n", match)
-					continue
+			doc.Find("tr.text_lauftext_shop").Each(func(i int, s *goquery.Selection) {
+				// For each item found, get the band and title
+				artikel := s.Find(".spalteArtikel1 > a").Text()
+
+				paranLeft := strings.LastIndex(artikel, "(")
+				if paranLeft < 0 {
+					log.Printf("KatWorker[%s] Artikel[%s] No left paren\n.", kat, artikel)
+					return
 				}
 
-				// convert artNr to int
-				artnr, err := strconv.Atoi(match[1])
+				paranRight := strings.LastIndex(artikel, ")")
+				if paranRight < 0 {
+					log.Printf("KatWorker[%s] Artikel[%s] No right paren\n.", kat, artikel)
+					return
+				}
+
+				artnrStr := artikel[paranLeft+8 : paranRight]
+				if len(artnrStr) == 0 {
+					log.Printf("KatWorker[%s] Artikel[%s] No artikelNr\n.", kat, artikel)
+					return
+				}
+
+				artnr, err := strconv.Atoi(artnrStr)
 				if err != nil {
-					log.Printf("KatWorker[%s] Match[%s] Error:%s", kat, match, err)
-					break
+					log.Printf("KatWorker[%s] Artikel[%s] Error:%s", kat, artikel, err)
+					return
 				}
 
-				// split cost string into ints
-				preisParts := strings.Split(match[2], ".")
-				if len(preisParts) != 2 {
-					log.Printf("len(preisPart) != 2 ! %v\n", preisParts)
-					continue
+				// ugly hack because bode remove the class ".spalteArtikel3"
+				subSel := s.Find("td")
+				if subSel.Length() < 4 {
+					log.Printf("SubSel <3 for artikel: %s\nSubSel:%v", artikel, subSel)
+					return
 				}
+
+				preis := subSel.Eq(3).Text()
+
+				// split preis by the decimal poin
+				preisParts := strings.Split(preis, ".")
+				if len(preisParts) != 2 {
+					log.Printf("len(preisPart) != 2 ! %v\n", preis)
+					return
+				}
+
 				hunderter, err := strconv.Atoi(preisParts[0])
 				if err != nil {
-					log.Printf("KatWorker[%s] Match[%s] Error:%s", kat, match, err)
-					break
+					log.Printf("KatWorker[%s] Artikel[%s] Error:%s", kat, artikel, err)
+					return
 				}
 
-				zehner, err := strconv.Atoi(preisParts[1])
+				// remove the malencoded euro sign from the end
+				zehner, err := strconv.Atoi(preisParts[1][:len(preisParts[1])-1])
 				if err != nil {
-					log.Printf("KatWorker[%s] Match[%s] Error:%s", kat, match, err)
-					break
+					log.Printf("KatWorker[%s] Artikel[%s] Error:%s", kat, artikel, err)
+					return
 				}
 
-				out <- Artikel{artnr, hunderter*100 + zehner, kat}
-			}
+				out <- Artikel{
+					Kategorie: kat,
+					Name:      artikel,
+					ArtNr:     artnr,
+					Preis:     hunderter*100 + zehner,
+				}
+			})
 		}
 		close(out)
 	}()
